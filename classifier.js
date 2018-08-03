70bed6217f9b3e6bfa8a238b313e26d0a7296353
Banjo Oluwapelumi <pblead98@yahoo.com>
2018-08-03 23:54:17 +0100

    Question 1

--- classifier.js
+++ classifier.js
@@ -1,74 +1,146 @@
-const classifier = require('../src/classifier');
-const input = require('../src/inputs/input');
-const output = require('../src/outputs/output');
-const inputEdge = require('../src/inputs/edge-input');
-const outputEdge = require('../src/outputs/out-edge');
+/**
+ * This is the entry point to the program
+ *
+ * @param {array} input Array of student objects
+ */
+function classifier(inputData) {
+  // Your code should go here.
 
-describe('classifier assumptions', () => {
-  test('it throws error given invalid input', () => {
-    expect(() => classifier()).toThrow();
-    expect(() => classifier('1')).toThrow();
-    expect(() => classifier({})).toThrow();
-  });
+       var input=undefined;
+        if( inputData instanceof Array){
+                input =[];var clone=function(v){input.push(v)};
+                inputData.forEach(clone); 
+        }else if(inputData instanceof String){
+           input=new String(inputData).toString();
+      }else  if(typeof inputData =='object'){
 
-  test('it does not mutate the original array', () => {
-    const immutable = require('../src/inputs/input');
-    Object.freeze(immutable);
+         input={};
+        for (var property in inputData){input[property] = inputData[property];}
 
-    expect(() => classifier(immutable)).not.toThrow();
-    expect(classifier(immutable)).toBeDefined();
-  });
 
-  test('it does not throw for empty arrays', () => {
-    const arr = [];
+        }
+ 
+ 
+  var len= input.length;
+  var result = {};
+  result["noOfGroups"] = 0;
 
-    expect(() => classifier(arr)).not.toThrow();
-    expect(classifier(arr)).toEqual({ noOfGroups: 0 });
-  });
+  if(input instanceof Array)
+  {
 
-  test('it makes no assumptions about the dob', () => {
-    const arr = [
-      {
-        name: 'Patrick',
-        dob: new Date('2015-10-15'),
-        regNo: '19',
-      },
-    ];
+  	if(input.length > 0)
+  	{
+  	  for (var i = 0; i < len ; i++)
+  {
+    var birthDate = new Date(input[i].dob);
+    var birthYear = birthDate.getUTCFullYear();
+    var birthMonth = birthDate.getUTCMonth();
+    var birthDay = birthDate.getUTCDate();
+    var currentDate = new Date();
+        var currentYear = currentDate.getFullYear();
+        var currentMonth = currentDate.getUTCMonth();
+        var currentDay = currentDate.getUTCDate();
+    var age = currentYear - birthYear;
+    if (currentMonth < birthMonth|| currentMonth == birthMonth && currentDay < birthDay)
+    { 
+    	input[i]["age"] = age--;
+    }
+    else
+    {
+    	input[i]["age"]=age;
+    }
+  }
 
-    expect(() => classifier(arr)).not.toThrow();
-    expect(classifier(arr)).toBeDefined();
-  });
-});
+     input.sort(function(a, b) { return a.age - b.age; },0);
 
-describe('classifier spec', () => {
-  test('it works for single value arrays', () => {
-    const arr = [
-      {
-        name: 'Patrick',
-        dob: new Date('2015-10-15'),
-        regNo: '19',
-      },
-    ];
+     var min,max,
+         num = 1;
+         groupName = "group"+num;
 
-    expect(classifier(arr)).toHaveProperty('noOfGroups', 1);
-    expect(classifier(arr)).toHaveProperty('group1.members');
-  });
+     min = input[0].age;
+     for (var i = 0; i < len ; i++)
+    { 
+        
 
-  test('partial values - some key names and values', () => {
-    const out = classifier(input);
+        if(!result.hasOwnProperty(groupName))
+        {
+          result[groupName] = {};
+          result[groupName]["members"] = [];          
+        }
 
-    expect(out).toMatchObject(output);
-  });
 
-  test('it gets everything right', () => {
-    const out = classifier(input);
+          max = input[i].age;
 
-    expect(out).toEqual(output);
-  });
 
-  test('passes for a different input', () => {
-    const out = classifier(inputEdge);
+         if((max - min) <= 5 && result[groupName]["members"].length <3)
+         {
 
-    expect(out).toEqual(outputEdge);
-  });
-});
+                  if(result[groupName]["members"].length == 0)
+                  {
+                     min = input[i].age;
+                     result[groupName]["members"].push(input[i]);
+                     result[groupName]["oldest"] = max;
+                     result[groupName]["sum"]= max;
+                       if(!result[groupName].hasOwnProperty("regNos"))
+                       {
+                          result[groupName]["regNos"] = [];                  
+                       }
+                     result[groupName]["regNos"].push(parseInt(input[i].regNo));
+                     result[groupName].regNos.sort(function(a, b) { return a - b; });
+                  }
+
+                  else 
+                  {  
+                     min = result[groupName]["members"][0].age;           
+                     result[groupName]["members"].push(input[i]);
+                     result[groupName]["oldest"] = max;
+                     result[groupName]["sum"]= result[groupName].sum + max;
+                       if(!result[groupName].hasOwnProperty("regNos"))
+                       {
+                          result[groupName]["regNos"] = [];                  
+                       }
+                     result[groupName]["regNos"].push(parseInt(input[i].regNo));
+                     result[groupName].regNos.sort(function(a, b) { return a - b; });
+                  } 
+               }
+
+       
+       else
+       { 
+
+             num++;
+             groupName = "group"+num;
+             result[groupName] = {};
+             result[groupName]["members"]= []; 
+             min =input[i].age;
+             max =input[i].age;
+             result[groupName]["members"].push(input[i]);
+             result[groupName]["oldest"] = max;
+             result[groupName]["sum"]= max;
+                 if(!result[groupName].hasOwnProperty("regNos"))
+                 {
+                    result[groupName]["regNos"] = [];                  
+                 }
+             result[groupName]["regNos"].push(parseInt(input[i].regNo));
+             result[groupName].regNos.sort(function(a, b) { return a - b; });
+       }
+       
+       result["noOfGroups"] = num;
+
+     }
+   }
+
+
+  	return result;
+  }
+
+  else
+  {
+  	 throw "invalid type";
+  }
+
+
+
+}
+
+module.exports = classifier;
